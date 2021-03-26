4d641b0b5a48ec62c2bdca53f73ca07a772d6563
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-26 18:03:15 -0300

    create DetailsFragment class

---
+++ app/src/main/java/com/camp/ioasys/ui/fragments/DetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.camp.ioasys.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.camp.ioasys.R
+import com.camp.ioasys.databinding.FragmentDetailsBinding
+import com.camp.ioasys.viewmodels.MainViewModel
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
