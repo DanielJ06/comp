0131649b89890fc5fec26bc5b38399f998185d0d
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-04-01 10:42:48 -0300

    Companies DAO

---
+++ app/src/main/java/com/camp/ioasys/CompaniesDao.kt
@@ -0,0 +1,18 @@
+package com.camp.ioasys
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CompaniesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCompanies(companiesEntity: CompaniesEntity)
+
+    @Query("SELECT * FROM companies_table")
+    fun readCompanies(): Flow<List<CompaniesEntity>>
+
+}
